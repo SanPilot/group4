9979da8ed0f46ab60994a86f3060cea3cb59e5f1
Hussain Khalil <contact@hussain.cf>
2015-04-10 14:58:20 -0700

    Work on new page style

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
+var paths = {
+	"introduction": "/",
+	"about": "/rabbits",
+	"what": "/whatisaninvasivespecies",
+	"introduction": "/",
+	"introduction": "/",
+	"introduction": "/",
+};
+function changePage() {
+
+}
+function getPageContent(path, attempt) {
+	var result;
+	if(attempt > 25) {
+		return false;
+	}
+	$.ajax("/content/"+path, {
+		success: function(xhr) {
+			result =JSON.parse(xhr.responseText);
+		},
+		error: function(){
+			getPageContent(path, ++attempt);
+		}
+	});
+}
+var currentpage = paths.indexOf(window.location.pathname.replace(/[^\\\/]*$/, ''));
 $(document).ready(function() {
+
 	setTimeout(function() {
 		$("#supercurtain").css("opacity","0");
 	}, 0);
