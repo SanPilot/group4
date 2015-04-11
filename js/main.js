c184f2c6d9861c3a769decb45bff21245295df18
Hussain Khalil <contact@hussain.cf>
2015-04-11 14:58:33 -0700

    Added fade in and fade out

--- js/main.js
+++ js/main.js
@@ -1,32 +1,11 @@
-var paths = {
-	"introduction": "/",
-	"about": "/rabbits",
-	"what": "/whatisaninvasivespecies",
-	"introduction": "/",
-	"introduction": "/",
-	"introduction": "/",
-};
-function changePage() {
-
-}
-function getPageContent(path, attempt) {
-	var result;
-	if(attempt > 25) {
-		return false;
-	}
-	$.ajax("/content/"+path, {
-		success: function(xhr) {
-			result =JSON.parse(xhr.responseText);
-		},
-		error: function(){
-			getPageContent(path, ++attempt);
-		}
-	});
-}
-var currentpage = paths.indexOf(window.location.pathname.replace(/[^\\\/]*$/, ''));
 $(document).ready(function() {
-
 	setTimeout(function() {
 		$("#supercurtain").css("opacity","0");
+		setTimeout(function() {
+			$("#supercurtain").css("display","none");
+		} ,1000)
 	}, 0);
+});
+$(document).on("beforeunload", function() {
+	$("#supercurtain").css("display","none").css("opacity","1");
 });
