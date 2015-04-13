c962ba042ef8271dbaf13d2cd89f938e724ed97f
Hussain Khalil <contact@hussain.cf>
2015-04-12 17:36:07 -0700

    Completed Rabbit Invasion Page

--- js/slider.js
+++ js/slider.js
@@ -2,7 +2,8 @@ $(window).load(function() {
 	$('#slider').bxSlider({
 		auto: true,
 		autoStart: true,
-		captions: true
+		captions: true,
+		easing: "ease-in-out",
 	});
 	$("#slider > li > img").on("mouseover", function() {
 		$(this).attr("title","");
