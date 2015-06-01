41f5d59ef33f6af41b640515c04dcd9cd530a1b8
Hussain Khalil <hussainpilot05@gmail.com>
2015-05-31 22:42:01 -0700

    Added move message

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
+function createPopup() {
+	$("body").prepend("<div id='movedwrapper'><div id='moved'><span id='movedheader'>Important Notice</span><br>Rabbitsaninvasivespecies.com has moved to rabbitsaninvasivespecies.cf (.com to .cf).<br> In the future, rabbitsaninvasivespecies.com will no longer work.<div id='movedinputs'><input type='checkbox' id='movedcheck'><label for='movedcheck' id='movedchecklabel'>Don't show me this again</label><button id='movedbtn'>Okay</button></div></div></div>");
+}
+$(document).ready(function() {
+	if(!getCookie("moved")) {
+		createPopup();
+	}
+	$("#movedbtn").on("click", function() {
+		$("#movedwrapper").css("opacity", "0");
+		setTimeout(function() {
+			$("#movedwrapper").css("display", "none");
+		}, 400);
+		if($("#movedcheck").is(":checked")) {
+			createCookie("moved","true",36500);
+		} else {
+			createCookie("moved","true");
+		}
+	});
+});
 $(window).load(function() {
 	setTimeout(function() {
 		$("#supercurtain").css("opacity","0");
@@ -9,3 +28,24 @@ $(window).load(function() {
 $(window).bind("beforeunload", function() {
 	$("#supercurtain").css("display","block").css("opacity","1");
 });
+var createCookie = function(name, value, days) {
+	var expires;
+	if (days) {
+		var date = new Date();
+		date.setTime(date.getTime() + (days * 24 * 60 * 60 * 1000));
+		expires = "; expires=" + date.toGMTString();
+	}
+	else {
+		expires = "";
+	}
+	document.cookie = name + "=" + value + expires + "; path=/";
+};
+var getCookie = function(c_name) {
+	if (document.cookie.length > 0) {
+		var c_start = document.cookie.indexOf(c_name + "=");
+		if (c_start != -1) {
+			return true;
+		}
+	}
+	return false;
+};
